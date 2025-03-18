2e4af6006b9e29c6ba826f7226d257084e9f665d
Omid Afroozeh <afroozeh.omid@gmail.com>
2025-03-18 13:17:09 +0100

    clickbench

--- experiments/duckdb_ussr_clickbench.py
+++ experiments/duckdb_ussr_clickbench.py
@@ -4,28 +4,27 @@ import sys
 root_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 sys.path.insert(0, root_directory)
 
-from config.benchmark.tpcds import get_tpcds_benchmark
-from config.systems.duckdb import DUCK_DB_FIRST_TEST, DUCK_DB_NIGHTLY_BUILD_LOCALLY
+from config.benchmark.clickbench import get_clickbench
+from config.systems.duckdb import DUCK_DB_USSR, DUCK_DB_MAIN
 from src.models import RunConfig
 from src.runner.experiment_runner import run
 
 
 def main():
-    sfs = [10, 30, 100, 300]
     config: RunConfig = {
-        'name': 'USSR_vs_baseline_tpcds',
+        'name': 'USSR_vs_baseline_clickbench',
         'run_settings': {
             'n_parallel': 1,
-            'n_runs': 3,
+            'n_runs': 5,
         },
         'system_settings': [
-            {'n_threads': 8},
+            {'n_threads': 6},
             # {'n_threads': 2},
             # {'n_threads': 4},
             # {'n_threads': 8},
         ],
-        'systems': [DUCK_DB_FIRST_TEST, DUCK_DB_MAIN],
-        'benchmarks': get_tpcds_benchmark(sfs),
+        'systems': [DUCK_DB_MAIN],
+        'benchmarks': get_clickbench(),
     }
     run(config)
 
