3a83701f1d1c4d42cce79bacdae8519dc116709f
Omid Afroozeh <omid@diamonds4.scilens.private>
2025-04-15 10:43:04 +0200

    idk 3.2

--- experiments/duckdb_ussr_microbenchmark_strings_variable_length.py
+++ experiments/duckdb_ussr_microbenchmark_strings_variable_length.py
@@ -5,26 +5,26 @@ root_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pard
 sys.path.insert(0, root_directory)
 
 from config.benchmark.microbenchmark_strings_variable_length import get_string_len_benchmark
-from config.systems.duckdb import DUCK_DB_USSR, DUCK_DB_MAIN, DUCK_DB_USSR_refactored, DUCK_DB_USSR_memcpy_optimization, DUCK_DB_USSR_new_lock, DUCK_DB_USSR_predicate_materialize_once, DUCK_DB_USSR_no_singleton, DUCK_DB_USSR_no_singleton_new_api
+from config.systems.duckdb import DUCK_DB_USSR_upper_limit, DUCK_DB_USSR_stable_version, DUCK_DB_USSR, DUCK_DB_MAIN, DUCK_DB_USSR_refactored, DUCK_DB_USSR_memcpy_optimization, DUCK_DB_USSR_new_lock, DUCK_DB_USSR_predicate_materialize_once, DUCK_DB_USSR_no_singleton, DUCK_DB_USSR_no_singleton_new_api
 from src.models import RunConfig
 from src.runner.experiment_runner import run
 
 
 def main():
-    sfs = [8, 16, 32, 64, 128, 256, 512]
+    sfs = [8, 16, 32, 64, 128, 255, 512]
     config: RunConfig = {
         'name': 'USSR_vs_baseline_microbenchmark_variable_length',
         'run_settings': {
-            'n_parallel': 2,
-            'n_runs': 15,
+            'n_parallel': 5,
+            'n_runs': 5,
         },
         'system_settings': [
-            # {'n_threads': 1},
+            {'n_threads': 1},
             # {'n_threads': 2},
             # {'n_threads': 4},
-            {'n_threads': 8},
+            # {'n_threads': 8},
         ],
-        'systems': [DUCK_DB_USSR_no_singleton_new_api, DUCK_DB_USSR_no_singleton],
+        'systems': [DUCK_DB_USSR_stable_version, DUCK_DB_MAIN, DUCK_DB_USSR_no_singleton],
         'benchmarks': get_string_len_benchmark(sfs),
     }
     run(config)
