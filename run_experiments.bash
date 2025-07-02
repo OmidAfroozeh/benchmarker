890bf069bd03349d736d6f3ab806bf37102d30cb
Omid Afroozeh <afroozeh.omid@gmail.com>
2025-07-02 10:36:57 +0200

    tet5

--- run_experiments.bash
+++ run_experiments.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
+poetry lock
+poetry install
 
-nohup poetry run python experiments/UnifiedStringDictionary/duckdb_ussr_tpch.py > all_output_tpch.log 2>&1
-nohup poetry run python experiments/UnifiedStringDictionary/Aggregation/aggregate_variable_length.py > all_output_var_length_microbench.log 2>&1
-nohup poetry run python experiments/UnifiedStringDictionary/Aggregation/aggregate_variable_groupsize_skewed.py > all_output_var_grpsize_skewed_microbench.log 2>&1
-nohup poetry run python experiments/UnifiedStringDictionary/Join/duckdb_ussr_jointest.py > all_output_jointest.log 2>&1
-
+poetry run python experiments/UnifiedStringDictionary/duckdb_ussr_tpch.py
+poetry run python experiments/UnifiedStringDictionary/Aggregation/aggregate_variable_length.py
+poetry run python experiments/UnifiedStringDictionary/Aggregation/aggregate_variable_groupsize_skewed.py
+poetry run python experiments/UnifiedStringDictionary/Join/duckdb_ussr_jointest.py
